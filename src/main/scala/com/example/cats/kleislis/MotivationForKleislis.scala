212e5b708b7e286314db45d1f790589948a478e7
Lucas Premuda <lucas.premuda@gmail.com>
2022-02-25 20:55:09 -0800

    Added MotivationForKleislis and text.txt file

--- src/main/scala/com/example/cats/kleislis/MotivationForKleislis.scala
+++ src/main/scala/com/example/cats/kleislis/MotivationForKleislis.scala
@@ -1,5 +1,24 @@
 package com.example.cats.kleislis
 
-object MotivationForKleislis {
+object MotivationForKleislis extends App {
+
+  // Motivation: In this simple example, function composition is easy when
+  // using primitive data types and monadic types, such as List, Option, etc...
+
+  // compiled as: val double: Function1[Int,Int] = x => x * 2
+  val double: Int => Int = x => x * 2
+  val intToCats: Int => String = x => {
+    if (x == 1) s"$x cat"
+    else s"$x cats"
+  }
+
+  println(double(3))
+  println(intToCats(1))
+  println(intToCats(4))
+
+  val doubleTheCatsAndThen: Int => String = double.andThen(intToCats)
+  println(doubleTheCatsAndThen(5))
+  val doubleTheCatsCompose: Function1[Int,String] = intToCats.compose(double)
+  println(doubleTheCatsCompose(5))
 
 }
