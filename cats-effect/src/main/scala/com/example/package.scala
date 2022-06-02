30a5dfd9acf2123a384057859326a656f8c8df57
Lucas Premuda <lucas.premuda@gmail.com>
2022-06-02 12:20:56 -0700

    Cats Effect MonadCancel exercise

--- cats-effect/src/main/scala/com/example/package.scala
+++ cats-effect/src/main/scala/com/example/package.scala
@@ -1,6 +1,10 @@
 package com
 
-import cats.effect.IO
+import cats.Functor
+import cats.syntax.functor._
+import cats.effect.{IO, MonadCancel}
+
+import scala.concurrent.duration.FiniteDuration
 
 package object example {
 
@@ -9,10 +13,25 @@ package object example {
     def debug: IO[A] = {
       for {
         a <- ioa
-        t = (Thread.currentThread().getName)
+        t = Thread.currentThread().getName
         _ = println(s"[$t] $a")
       } yield a
     }
+  }
+
+  implicit class DebugWrapper2[F[_], A](fa: F[A]) {
+    def debug2(implicit functor: Functor[F]): F[A] = {
+      fa.map { a =>
+        val t = Thread.currentThread().getName
+        println(s"[$t] $a")
+        a
+      }
+    }
+
+    def unsafeSleep[E](finiteDuration: FiniteDuration)(implicit mc: MonadCancel[F, E]): F[Unit] = {
+      mc.pure(Thread.sleep(finiteDuration.toMillis))
+    }
+
   }
 
 }
